fc3bd67b459f27999cba76ef0965ee1161e72bb1
Advik <advik.b@gmail.com>
2022-07-01 21:49:36 +0530

    Define some basic constants to be used within the app

---
+++ util/constants.py
@@ -0,0 +1,4 @@
+ERROR = "[ [bold red]ERROR[/bold red] ]:"
+DEBUG = "[ [bold cyan]DEBUG[/bold cyan] ]:"
+WARNING = "[ [bold yellow]WARNING[/bold yellow] ]:"
+INFO = "[ [bold green]INFO[/bold green] ]:"
