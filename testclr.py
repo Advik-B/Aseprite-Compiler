9369d14bfef02ea1b5ba652e3e77111403a8a289
Advik <advik.b@gmail.com>
2022-07-01 20:35:26 +0530

    Update testclr.py

--- testclr.py
+++ testclr.py
@@ -1,10 +1,33 @@
-from supports_color import supportsColor
+from util.scaffolding import console
+from rich.terminal_theme import TerminalTheme
 
-if supportsColor.stdout:
-    print('Terminal stdout supports color');
+discord = TerminalTheme(
+    (39, 41, 46),
+    (255, 255, 255),
+[
+        (0, 0, 0),
+        (0, 200, 0),
+        (0, 200, 0),
+        (255, 255, 0),
+        (100, 100, 255),
+        (128, 0, 128),
+        (0, 255, 255),
+        (160, 187, 255),
+    ],
+    [
+        (160, 187, 255),
+        (255, 0, 0),
+        (0, 255, 0),
+        (255, 255, 0),
+        (0, 0, 255),
+        (255, 141, 255),
+        (0, 255, 255),
+        (255, 255, 255),
+    ],
+)
 
-if supportsColor.stdout.has256:
-    print('Terminal stdout supports 256 colors');
 
-if supportsColor.stderr.has16m:
-    print('Terminal stderr supports 16 million colors (truecolor)');
+
+console.log("TerminalTest initilized", log_locals=True)
+with open('out.html', 'wb') as f:
+    f.write(console.export_html(theme=discord).encode('utf-8'))
