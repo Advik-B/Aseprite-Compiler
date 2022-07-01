8b38a0fe5a3b176428b358009ee6376280cbd1a3
Advik <advik.b@gmail.com>
2022-07-01 16:33:48 +0530

    Update unused_temp_util.py

--- util/unused_temp_util.py
+++ util/unused_temp_util.py
@@ -1,3 +1,6 @@
+import sys
+import termcolor
+
 def buildblock(size):
     block = str()
     for _ in range(size):
@@ -22,3 +25,30 @@ def generate_fake_headers():
         "Connection": "keep-alive",
         "User-Agent": get("https://randua.deta.dev").json()["ua"],
     }
+
+
+
+LF = "\n"
+CRLF = "\r\n"
+
+# The current line mode. May be in the future I will make this script cross-platform.
+# For now, it is Windows-only.
+CURRENT_LINE_MODE = CRLF
+
+
+def cout(
+    *text: str,
+    color: str = "white",
+    styles: tuple = (),
+    on_color: str = "grey",
+    end: str = CURRENT_LINE_MODE
+) -> None:
+    """_summary_
+
+    Args:
+        color (str, optional): _description_. Defaults to "white".
+    """
+    for t in text:
+        sys.stdout.write(termcolor.colored(str(t), color, "on_" + on_color, styles))
+    sys.stdout.write(end)
+    sys.stdout.flush()
