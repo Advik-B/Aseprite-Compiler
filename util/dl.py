532c2292b016feffd5f6b27dab4d71b5ad86369f
Advik <advik.b@gmail.com>
2022-07-02 16:33:35 +0530

    Update dl.py

--- util/dl.py
+++ util/dl.py
@@ -1,10 +1,13 @@
-from .prime_numbers import is_prime
-from ..errors import NoInternetConnection
-from .o import console
-from .constants import ERROR, INFO
-from rich.console import Console
-from requests import get
 import json
+
+from requests import get
+from rich.console import Console
+
+from ..errors import NoInternetConnection
+from .constants import ERROR, INFO, WARNING
+from .o import console
+from .prime_numbers import is_prime
+
 
 def buildblock(size):
     block = str()
@@ -51,25 +54,26 @@ def download(
     check_internet_connection: bool = True,
     allow_redirects: bool = True,
     show_locals: bool = False,
-    ):
+):
     if check_internet_connection:
         if not check_internet_connection_():
             console.log(f"{ERROR} No internet connection.")
             return NoInternetConnection("Module: %s" % __name__)
-    
+
     headers = {}
     validate_ssl = False
     if blend_in:
         headers = generate_fake_headers()
     if be_careful:
         validate_ssl = True
-    
-    response = get(url, headers=headers, verify=validate_ssl, allow_redirects=allow_redirects)
+
+    response = get(url, headers=headers, verify=validate_ssl,
+                   allow_redirects=allow_redirects)
 
     if show_headers:
         console.log(INFO, " Headers: ")
         console.print_json(json.dumps(response.headers), indent=4)
-    
+
     if show_extra_info:
         console.log(INFO, " Extra info: ")
         console.print_json(
@@ -81,22 +85,33 @@ def download(
                     "encoding": response.encoding,
                     "content_type": response.headers.get("content-type"),
                     "history": response.history,
-                        
+
                 },
                 indent=4,
                 ensure_ascii=False)
-            )
-    
+        )
+
     # Get the filename from the response headers if possible.
     filename = fallback_filename
     if fallback_filename is None:
         try:
-            filename = response.headers.get("content-disposition").split("filename=")[1]
+            filename = response.headers.get(
+                "content-disposition").split("filename=")[1]
         except IndexError:
             pass
-    
+
     if filename is None:
         filename = url.split("/")[-1]
-    
+
     if show_locals:
         console.log(log_locals=True)
+
+    file_size = int(response.headers.get("content-length", 0))
+    if file_size < 1024:
+        console.log(WARNING, f"File size: {file_size} bytes (less than 1MB)")
+
+    chunk_dl = True
+    if file_size >= 0:
+        console.log(
+            WARNING, f"File size is not known. Chunk downloading is not possible.")
+        chunk_dl = False
