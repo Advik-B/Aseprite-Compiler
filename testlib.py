903a5cb9a685d90e4508150f67410a662d8c3045
Advik <advik.b@gmail.com>
2022-07-02 19:41:02 +0530

    Update testlib.py

--- testlib.py
+++ testlib.py
@@ -1,3 +1,3 @@
-from util import download
-
-download("https://github.com/Griphitor/Griph-pad/releases/download/v1.0.1-beta/Griphitor-win.zip", be_careful=True)
+from util import download, console, discord_theme
+download("https://github.com/Griphitor/Griph-pad/releases/download/v1.0.1-beta/xref-main.html", show_extra_info=True, show_locals=True, show_headers=True)
+# console.save_html('Output.html', theme=discord_theme)
