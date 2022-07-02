acdeeeed097e70aedf02c6761f1f5512e786aef5
Advik <advik.b@gmail.com>
2022-07-02 19:42:08 +0530

    Separate the file-iter-writing part
     To avoid nested hell

---
+++ util/_dl.py
@@ -0,0 +1,14 @@
+from alive_progress import alive_bar
+from io import BytesIO
+from requests import Response
+from functools import partial
+
+def iter_content(file: BytesIO, conten_length:int,r: Response, chunk_size=1024):
+    """Iterates over the response data in chunks of bytes."""
+    # check if the file-size not divisible by chunk-size.
+    if conten_length % chunk_size != 0:
+        chunk_size = conten_length % chunk_size
+    with alive_bar(conten_length // chunk_size) as bar:
+        for data in r.iter_content(chunk_size=chunk_size):
+            file.write(data)
+            bar()
