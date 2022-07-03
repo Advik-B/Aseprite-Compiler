5d0ada18ee56162cf88047c9ff55ce0745ab091d
Advik <advik.b@gmail.com>
2022-07-03 22:45:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from util import (
     errors,
 )
 from rich.tree import Tree
+
 startup_message = (
 
 "[bold green]Welcome[/] to [bold purple][link=https://github.com/Advik-B/Aseprite-Compiler]Aseprite-Compiler[/][/].\n"
@@ -16,13 +17,20 @@ startup_message = (
 # "   - [bold green]Download[/] Aseprite source code from [italic bold white link=https://github.com]github[/] using [italic bold red]git[/]\n"
 )
 
-workflow = Tree(label="")
-workflow.add('[italic bold red][link=https://git-scm.com]Git[/][/]')
-workflow.add('[italic bold blue]Aseprite (source code)[/]')
-workflow.add('CMake')
+workflow = Tree(label="There are the dependencies that Aseprite-Compiler needs to run.")
+workflow.add('[italic bold red link=https://git-scm.com]Git[/] [bold white](Version Control System)[/]')
+workflow.add('[italic bold blue link=https://github.com/Aseprite/Aseprite]Aseprite[/] [bold white](Source-Code)[/]')
+workflow.add('[italic bold green link=https://github.com/aseprite/skia]Skia[/] [bold white](Library)[/]')
+workflow.add('[italic bold cyan link=https://cmake.org]CMake[/] [bold white](Build-System)[/]')
+workflow.add('[italic bold purple link=https://ninja-build.org/]Ninja[/] [bold white](Build-System/Assembler)[/]')
+workflow.add('[italic bold magenta link=https://visualstudio.microsoft.com/downloads/]Visual Studio Build Tools[/] [bold white](Compiler)[/]')
+# workflow.add('[italic bold white link=https://visualstudio.microsoft.com/downloads]Microsoft Visual C++ Redistributable[/] [bold white](Compiler)[/]')
+workflow.add('[italic bold Yellow link=https://developer.microsoft.com/en-us/windows/downloads/windows-sdk/]Windows SDK[/] [bold white](Software Development Kit)[/]')
 
 console.print(
     startup_message,
     workflow,
     highlight=True,
 )
+
+raise errors.NoInternetConnection("No Internet Connection")
