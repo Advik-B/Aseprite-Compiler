7c06f44a919b9e8e62b2bc63aab2f479f41b4762
Advik <advik.b@gmail.com>
2022-07-03 16:53:14 +0530

    Update test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+from util import console, discord_theme, download
+from rich import inspect
+from rich.markdown import Markdown
+from rich.progress import track
+from time import sleep
+
+download('https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.0.18/SDL2_ttf-2.0.18.zip')
