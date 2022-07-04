e39c2af9771909f2c67bacc1fe0b66c58b525a7d
Advik <advik.b@gmail.com>
2022-07-04 19:47:52 +0530

    E501: Comply with pep8 style guide

--- util/__init__.py
+++ util/__init__.py
@@ -1,17 +1,14 @@
 # Install the rich traceback first before anything else.
+# I am explicitly importing these constants because it helps with type
+# hints and Tests.
+from .constants import (ERROR, DEBUG, WARNING, INFO, )
+from .dl import download
+from rich.terminal_theme import TerminalTheme
 from rich.traceback import install
 from .o import console
 
 install(extra_lines=5, show_locals=True, console=console)
 
-from rich.terminal_theme import TerminalTheme
-from .dl import download
-from .constants import (
-    ERROR,
-    DEBUG,
-    WARNING,
-    INFO,
-)  # I am explicitly importing these constants because it helps with type hints and Tests.
 
 discord_theme = TerminalTheme(
     (39, 41, 46),
