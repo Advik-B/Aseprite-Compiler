737f935b77e3dc49446e34e0d557c371ba882295
Sourcery AI <>
2022-07-04 13:53:20 +0000

    'Refactored by Sourcery'

--- util/dl.py
+++ util/dl.py
@@ -65,18 +65,12 @@ def download(
     if url[-1] == "/":
         url = url[:-1]
 
-    if check_internet_connection:
-        if not check_internet_connection_():
-            console.log(f"{ERROR} No internet connection.")
-            return NoInternetConnection("Module: %s" % __name__)
+    if check_internet_connection and not check_internet_connection_():
+        console.log(f"{ERROR} No internet connection.")
+        return NoInternetConnection(f"Module: {__name__}")
 
-    headers = {}
-    validate_ssl = False
-    if blend_in:
-        headers = generate_fake_headers()
-    if be_careful:
-        validate_ssl = True
-
+    headers = generate_fake_headers() if blend_in else {}
+    validate_ssl = be_careful
     response = get(
         url,
         headers=headers,
@@ -104,14 +98,11 @@ def download(
 
     # Get the filename from the response headers if possible.
     filename = fallback_filename
-    if fallback_filename is None:
+    if filename is None:
         try:
             filename = response.headers.get("content-disposition").split("filename=")[1]
-        except IndexError:
+        except (IndexError, AttributeError):
             pass
-        except AttributeError:
-            pass
-
     if filename is None:
         filename = url.split("/")[-1]
         console.log(WARNING, f"No filename found. Using {filename}.")
@@ -128,8 +119,10 @@ def download(
     chunk_dl = True
     if file_size <= 0:
         console.log(
-            WARNING, f"File size is not known. Chunk downloading is not possible."
+            WARNING,
+            "File size is not known. Chunk downloading is not possible.",
         )
+
         chunk_dl = False
 
     chunk_size = 1024
@@ -137,17 +130,10 @@ def download(
     if file_size % chunk_size != 0:
         chunk_size = file_size % chunk_size
 
-    # file_size = file_size / chunk_size
-    if chunk_dl:
-        if show_progress:
-            with open(filename, "wb") as f:
-                iter_content(f, file_size, response, progressbar=progressbar)
+    with open(filename, "wb") as f:
+        if chunk_dl and show_progress:
+            iter_content(f, file_size, response, progressbar=progressbar)
         else:
-            with open(filename, "wb") as f:
-                for data in response.iter_content(chunk_size=chunk_size):
-                    f.write(data)
-    else:
-        with open(filename, "wb") as f:
             for data in response.iter_content(chunk_size=chunk_size):
                 f.write(data)
     if show_locals:
