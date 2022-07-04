c997cf9670a8267717f34b70ff4a8dd39eb005f3
Advik <advik.b@gmail.com>
2022-07-04 19:04:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,7 @@ from rich import inspect
 from rich.markdown import Markdown
 from time import sleep
 
-download('https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.0.18/SDL2_ttf-2.0.18.zip', progressbar='rich')
+download(
+    "https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.0.18/SDL2_ttf-2.0.18.zip",
+    progressbar="rich",
+)
