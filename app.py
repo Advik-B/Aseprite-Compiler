5d2dfe4201ceb424b6229bb487b49da7a5039084
Advik <advik.b@gmail.com>
2022-07-04 21:14:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ console.push_theme(custom_style)
 startup_message = (
     "[bold green]Welcome[/] to [bold purple][link=https://github.com/Advik-B/Aseprite-Compiler]Aseprite-Compiler[/][/].\n"
     "[bold purple]Aseprite-Compiler[/] is a CLI tool for compiling [bold blue link=https://www.aseprite.org]Aseprite[/] from its [italic cyan link=https://github.com/Aseprite/Aseprite]source code.[/]\n"
-    "Here is come information cause I dont know what-else to put here :grinning_face_with_sweat:😅\n")
+    "Here is come information cause I dont know what-else to put here :grinning_face_with_sweat:\n")
 
 deps = Panel(
     "1. [italic bold red link=https://git-scm.com]Git[/] [bold white](Version Control System)[/]\n"
@@ -29,7 +29,7 @@ deps = Panel(
     "5. [italic bold purple link=https://ninja-build.org/]Ninja[/] [bold white](Build-System/Assembler)[/]\n"
     "6. [italic bold magenta link=https://visualstudio.microsoft.com/downloads/]Visual Studio Build Tools[/] [bold white](Compiler)[/]\n"
     "7. [italic bold Yellow link=https://developer.microsoft.com/en-us/windows/downloads/windows-sdk/]Windows SDK[/] [bold white](Software Development Kit)[/]\n",
-    title="There are the dependencies that Aseprite-Compiler needs.",
+    title="There are the dependencies that Aseprite-Compiler needs. (No need to install them :winking_face:)",
     highlight=True,)
 
 steps = Panel(
@@ -45,3 +45,5 @@ console.print(
     steps,
     highlight=True,
 )
+
+console.print("[italic]Anyways[/], let's start with some basic questions.")
