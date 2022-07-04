5ba97847a0252c526234098388afccb0daae9a7d
Advik <advik.b@gmail.com>
2022-07-04 17:44:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,15 @@ deps.add('[italic bold purple link=https://ninja-build.org/]Ninja[/] [bold white
 deps.add('[italic bold magenta link=https://visualstudio.microsoft.com/downloads/]Visual Studio Build Tools[/] [bold white](Compiler)[/]')
 deps.add('[italic bold Yellow link=https://developer.microsoft.com/en-us/windows/downloads/windows-sdk/]Windows SDK[/] [bold white](Software Development Kit)[/]')
 
+
+steps = Tree(label="There are the details on how [bold purple]Aseprite-Compiler[/] works.")
+steps.add("Check if any of the dependencies are missing on your system. [italic green][bold white]([/]Usally [cyan]fast[/][bold white])[/][/]")
+steps.add("Download the dependencies if they are missing. [italic yellow][bold white]([/]This will take [italic white]some[/] time[bold white])[/][/]")
+steps.add("Temporarily install the dependencies. [italic yellow][bold white]([/]This will take [italic white]some[/] time[bold white])[/][/]")
+steps.add("Compile Aseprite. [italic yellow][bold white]([/]This will take [bold white]A LOT[/] of time[bold white])[/][/]")
 console.print(
     startup_message,
     deps,
+    steps,
     highlight=True,
 )
