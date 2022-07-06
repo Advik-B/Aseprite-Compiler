ae632830ed3bfa9962c32bc55325b8347b6db165
Advik <advik.b@gmail.com>
2022-07-06 20:15:02 +0530

    Update __init__.py

--- util/__init__.py
+++ util/__init__.py
@@ -1,13 +1,41 @@
 # Install the rich traceback first before anything else.
 # I am explicitly importing these constants because it helps with type
 # hints and Tests.
-from .constants import (ERROR, DEBUG, WARNING, INFO, )
-from .dl import download
 from rich.terminal_theme import TerminalTheme
 from rich.traceback import install
+
+from .constants import DEBUG, ERROR, INFO, WARNING
+from .dl import download
 from .o import console
 
 install(extra_lines=5, show_locals=True, console=console)
+
+import os
+import platform
+
+OS = platform.system().casefold()
+
+
+def find_executable_in_path(executable, multiple=False):
+    """
+    Finds the path of an executable.
+    """
+    if OS == "windows" and not executable.endswith(".exe"):
+        executable += ".exe"
+    executables = []
+    path = os.environ["PATH"]
+    for directory in path.split(os.pathsep):
+        if os.path.exists(os.path.join(directory, executable)):
+            executables.append(os.path.join(directory, executable))
+
+    if len(executables) == 0:
+        return None
+
+    if multiple:
+        return tuple(executables)
+    else:
+        return executables[0]
+
 
 
 discord_theme = TerminalTheme(
